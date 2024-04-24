4fa29a23dd9f129f72a7367195ee084a8f4cd5a3
cnbilly <cheuknam.liu17@gmail.com>
2024-04-24 15:22:53 +0800

    fix consumer config

--- src/main/java/com/example/transactionapi/config/KafkaConsumerConfig.java
+++ src/main/java/com/example/transactionapi/config/KafkaConsumerConfig.java
@@ -33,6 +33,8 @@ public class KafkaConsumerConfig {
         return new DefaultKafkaConsumerFactory<>(consumerConfig());
     }
 
+    @Bean
+
     public KafkaListenerContainerFactory<
             ConcurrentMessageListenerContainer<String, String>> factory (
             ConsumerFactory<String, String> consumerFactory
